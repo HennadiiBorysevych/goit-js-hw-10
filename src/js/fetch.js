e7ff61a7bc8f5d1b1fcd2e9145ddc6aec8faab56
HennadiiBorysevych <genuch3@gmail.com>
2023-02-13 19:54:24 +0200

    async refactoring

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,14 +1,8 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-export default function fetchCountries(countryname) {
-  return fetch(
+export default async function fetchCountries(countryname) {
+  const response = await fetch(
     `https://restcountries.com/v3.1/name/${countryname}?fields=name,capital,population,flags,languages`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(
-        Notify.failure('Oops, there is no country with that name')
-      );
-    }
-    return response.json();
-  });
+  );
+  return await response.json();
 }
